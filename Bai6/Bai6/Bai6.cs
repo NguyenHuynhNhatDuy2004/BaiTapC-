082e8f1a472c13c67219d173f0c26d77063fac69
CGDN-NguyenHuynhNhatDuy2004 <nhatduy3q@gmail.com>
2025-02-20 00:32:48 +0700

    them bai 6

---
+++ Bai6/Bai6/Bai6.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai6
+{
+	internal class Bai6
+	{
+		static void Main()
+		{
+			Console.WriteLine("Bài 6: " + TimBCNN(12, 18));
+		}
+		static int TimBCNN(int a, int b)
+		{
+			int gcd_104 = TimUCLN(a, b);
+			return (a * b) / gcd_104;
+		}
+		static int TimUCLN(int a, int b)
+		{
+			while (b != 0)
+			{
+				int temp = b;
+				b = a % b;
+				a = temp;
+			}
+			return a;
+		}
+	}
+}
