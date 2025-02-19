54ef82bcd22c92810b644675939e2b40328f93a2
CGDN-NguyenHuynhNhatDuy2004 <nhatduy3q@gmail.com>
2025-02-20 00:07:16 +0700

    Them bai 2

---
+++ Bai2/Bai2/bai2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+	internal class bai2
+	{
+		static void Main()
+		{
+			Console.WriteLine("Bài 2: " + TinhTong(5));
+		}
+		static long TinhTong(int n)
+		{
+			long sum_104 = 0, factorial = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				factorial *= i;
+				sum_104 += factorial;
+			}
+			return sum_104;
+		}
+	}
+}
