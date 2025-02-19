2412b83d6c7be2cc4fc7e96f0195be48a9f9c3f3
CGDN-NguyenHuynhNhatDuy2004 <nhatduy3q@gmail.com>
2025-02-20 00:40:36 +0700

    them bai7

---
+++ Bai7/Bai7/Basi7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai7
+{
+	internal class Basi7
+	{
+		static void Main()
+		{
+			Console.WriteLine("Bài 7: " + TinhS(2, 4));
+		}
+		static double TinhS(double x, int n)
+		{
+			double sum_104 = 0, sumDenominator = 0, powX = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				sumDenominator += i;
+				powX *= -x;
+				sum_104 += powX / sumDenominator;
+			}
+			return sum_104;
+		}
+	}
+}
